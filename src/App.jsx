c15699c7bfb0d5fda05b872d352c770a2380a9d1
MAndresT12 <89875869+MAndresT12@users.noreply.github.com>
2026-06-17 17:07:42 -0500

    edit app

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
-import { useEffect, useState } from 'react'
-import confetti from 'canvas-confetti'
-import './App.css'
-import { TURNS } from './constants.js'
-import {checkWinner, checkEndGame} from './logic/board.js'
-import { WinnerModal } from './components/WinnerModal.jsx'
-import { Board } from './components/Board.jsx'
-import { TurnIndicator } from './components/TurnIndicator.jsx'
-import { useGame } from './hooks/useGame.js'
+import "./App.css";
+import { WinnerModal } from "./components/WinnerModal.jsx";
+import { Board } from "./components/Board.jsx";
+import { TurnIndicator } from "./components/TurnIndicator.jsx";
+import { useGame } from "./hooks/useGame.js";
 function App() {
-
-  const {board, turn, winner, updateBoard, resetGame} = useGame()
+  const { board, turn, winner, updateBoard, resetGame } = useGame();
   //useState que al cambiar vuelve a renderizar componente para reflejar los cambios
-  //useEffect hook que permite ejecutar codigo arbitrario cuando componente se monta en el dom, y cada vez que cambian las dependencias que nosotros le digamos 
+  //useEffect hook que permite ejecutar codigo arbitrario cuando componente se monta en el dom, y cada vez que cambian las dependencias que nosotros le digamos
   // useEffect(() => {
   //   console.log('El ganador es: ', winner)
   // }, [winner]) //dependencia, cada vez que board cambie, se ejecutara el codigo dentro del useEffect
@@ -22,17 +17,13 @@ function App() {
     <main className="board">
       <h1>Tic tac toe</h1>
       <button onClick={resetGame}>Reset game</button>
-        <Board board={board} updateBoard={updateBoard}/>
+      <Board board={board} updateBoard={updateBoard} />
 
+      <TurnIndicator turn={turn} />
 
-      <TurnIndicator turn={turn}/>
-
-
-       <WinnerModal winner={winner} resetGame={resetGame}/>
-
+      <WinnerModal winner={winner} resetGame={resetGame} />
     </main>
-   
-  )
+  );
 }
 
-export default App
+export default App;
